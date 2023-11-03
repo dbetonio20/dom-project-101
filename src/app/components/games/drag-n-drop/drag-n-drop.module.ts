ec4606ffdd7d60a0f37742aa5ba14750d1a78b4f
Dominique Betonio <dominique@ereflect.com>
2023-11-04 02:21:15 +0800

    working module

--- src/app/components/games/drag-n-drop/drag-n-drop.module.ts
+++ src/app/components/games/drag-n-drop/drag-n-drop.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DragNDropComponent } from './drag-n-drop.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    DragDropModule
+    DragDropModule,
+    FormsModule,
   ],
   declarations: [DragNDropComponent],
   exports: [
     DragNDropComponent
-  ]
+  ],
 })
 export class DragNDropModule { }
